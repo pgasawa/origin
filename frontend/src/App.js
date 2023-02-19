be4715e962baa9df3d51cfd3694c6de464a92109
Ayushi Batwara <ayushibatwara@gmail.com>
2023-02-18 20:07:11 -0800

    some work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
-import React, { Component }  from 'react';
-import BasicCard from './components/card';
+import Grid from '@mui/material/Grid'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text } from 'react-native'
+import Cluster from './components/card';
 
 
 function App() {
+
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Button variant="contained">Contained</Button>
-        <BasicCard title="CS 189" description="Introduction to Machine Learning"></BasicCard>
-      </header>
+      <div className='Time'>{currentTime.toLocaleTimeString()}</div>
+      <div className='HelloMessage'></div>
+      <div className="Workspaces">
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+          <Cluster color={'red'} className="BasicCard" title="CS 189"></Cluster>
+          </Grid>
+          <Grid item xs={4}>
+          <Cluster className="BasicCard" title="CS 170"></Cluster>
+          </Grid>
+          <Grid item xs={4}>
+          <Cluster className="BasicCard" title="CS 61A"></Cluster>
+          </Grid>
+        </Grid>
+        </div>
     </div>
   );
 }
--- frontend/src/components/card.js
+++ frontend/src/components/card.js
@@ -1,42 +1,20 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
-
-export default function BasicCard(title, description) {
+export default function Cluster() {
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card sx={{ maxWidth: 300 }}>
       <CardContent>
-        {/* <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography> */}
         <Typography variant="h5" component="div">
-          title
+          CS 189
         </Typography>
-        {/* <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography> */}
-        <Typography variant="body2">
-          description
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <hr />
+        <Button size="medium">LAUNCH WORKSPACE</Button>
       </CardContent>
-      <CardActions>
-        <Button size="small">LAUNCH MODE</Button>
-      </CardActions>
     </Card>
   );
 }
