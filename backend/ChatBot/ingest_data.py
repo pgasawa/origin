9655ce8a1fd2d03d081901a984935c0f6ac62ee2
Jason Ding <jasonding@berkeley.edu>
2023-02-18 17:37:14 -0800

    Fix server

---
+++ backend/ChatBot/ingest_data.py
@@ -0,0 +1,32 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+import pickle
+import nltk
+import os
+import openai
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+def ingestion(id):
+    # Load Data
+    loader = UnstructuredFileLoader(f"{id}_output.txt")
+    raw_documents = loader.load()
+
+    # Split text
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    documents = text_splitter.split_documents(raw_documents)
+
+
+    # Load Data to vectorstore
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_documents(documents, embeddings)
+
+
+    # Save vectorstore
+    with open(f"vectorstore{id}.pkl", "wb") as f:
+        pickle.dump(vectorstore, f)
